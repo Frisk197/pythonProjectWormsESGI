b13fc434d3061aed75337b8ae70dfeddc8a2f77a
Frisk197 <bonnerondorian@gmail.com>
2024-02-17 14:46:27 +0100

    wip

--- main.py
+++ main.py
@@ -24,6 +24,36 @@ def characterMovements(key, character):
             character.move_ip(0, 2)
 
 
+def loadMainMenu(screen, teams):
+    runningMenu = True
+    while runningMenu:
+        screen.fill((0, 0, 0))
+        font = pygame.font.SysFont(None, 24)
+        img = font.render('' + teams, True, (255, 0, 0))
+        screen.blit(img, (20, 20))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        pygame.display.update()
+    return 2
+
+
+def loadGame(screen, teams):
+    runningGame = True
+    while runningGame:
+        screen.fill((0, 0, 0))
+        #game here
+        font = pygame.font.SysFont(None, 24)
+        img = font.render('c le jeu', True, (255, 0, 0))
+        screen.blit(img, (20, 20))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        pygame.display.update()
+    return 1
+
+
 pygame.init()
 
 SCREEN_WIDTH = 1920
@@ -34,18 +64,24 @@ pygame.display.set_caption("Worms ESGI")
 
 player = pygame.Rect((300, 250, 50, 50))
 
+scene = 1 # 1 for main menu, 2 for game
+
 running = True
 
 while running:
 
-    screen.fill((0, 0, 0))
+    teams = 1
 
-    pygame.draw.rect(screen, (255, 0, 0), player)
-
-    characterMovements(pygame.key.get_pressed(), player)
+    if scene == 1:
+        scene = loadMainMenu(screen, teams)
+    elif scene == 2:
+        scene = loadGame(screen, teams)
+    else:
+        pygame.quit()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.display.update()
+
+
 pygame.quit()
